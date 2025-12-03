fbb291914ff0ac03ea9bf9128734c05d8774865b
Istvan Viczian <viczian.istvan@gmail.com>
2025-12-03 14:58:29 +0100

    Aláírás ellenőrzés

--- jca/src/main/java/jca/CertificateMain.java
+++ jca/src/main/java/jca/CertificateMain.java
@@ -94,5 +94,14 @@ public class CertificateMain {
         try (var output = Files.newOutputStream(Path.of("training-keystore.p12"))) {
             keyStore.store(output, "secret".toCharArray());
         }
+
+        // Egy olyan kulcstár generálása amiben csak a tanúsítvány van benne
+        keyStore = KeyStore.getInstance("PKCS12");
+        keyStore.load(null, null); // Ha ez nincs, akkor nem inicializált, és exception-t dob
+        keyStore.setCertificateEntry("training-certificate", certificate);
+
+        try (var output = Files.newOutputStream(Path.of("training-keystore-just-certificate.p12"))) {
+            keyStore.store(output, "secret".toCharArray());
+        }
     }
 }
--- jca/src/main/java/jca/VerifySignMain.java
+++ jca/src/main/java/jca/VerifySignMain.java
@@ -5,29 +5,30 @@ import java.nio.file.Path;
 import java.security.KeyStore;
 import java.security.PrivateKey;
 import java.security.Signature;
-import java.util.HexFormat;
 
-public class SignMain {
+public class VerifySignMain {
 
     public static void main(String[] args) throws Exception {
+        // Dokumentum
         var data = "Hello World!".getBytes();
 
         var keyStore = KeyStore.getInstance("PKCS12");
 
-        try (var input = Files.newInputStream(Path.of("training-keystore.p12"))) {
+        try (var input = Files.newInputStream(Path.of("training-keystore-just-certificate.p12"))) {
             keyStore.load(input, "secret".toCharArray());
         }
 
-        var privateKey = (PrivateKey) keyStore.getKey("training", "secret".toCharArray());
+        var certificate = (java.security.cert.X509Certificate) keyStore.getCertificate("training-certificate");
 
         var signature = Signature.getInstance("SHA256withRSA");
-        signature.initSign(privateKey);
+        signature.initVerify(certificate.getPublicKey());
+
         signature.update(data);
-        var signatureBytes = signature.sign();
 
-        var hex = HexFormat.of();
-        System.out.println(hex.formatHex(signatureBytes));
+        var signatureBytes = Files.readAllBytes(Path.of("signature.bin"));
+        var valid = signature.verify(signatureBytes);
 
-        Files.write(Path.of("signature.bin"), signatureBytes);
+        System.out.println("Valid: " + valid);
+
     }
 }
