bbdcc822440d42073c07afc212e2821c67ec5685
Istvan Viczian <viczian.istvan@gmail.com>
2025-12-04 09:29:32 +0100

    Projekt HTTPS-hez

---
+++ hello-boot-https/src/main/java/training/helloboothttps/HelloBootHttpsApplication.java
@@ -0,0 +1,13 @@
+package training.helloboothttps;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloBootHttpsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloBootHttpsApplication.class, args);
+    }
+
+}
---
+++ hello-boot-https/src/test/java/training/helloboothttps/HelloBootHttpsApplicationTests.java
@@ -0,0 +1,13 @@
+package training.helloboothttps;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloBootHttpsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
