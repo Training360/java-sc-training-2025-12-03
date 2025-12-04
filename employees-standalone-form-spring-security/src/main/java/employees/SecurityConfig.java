0b674ef586c26eb6609249836fa7215905c1cf2e
Istvan Viczian <viczian.istvan@gmail.com>
2025-12-04 15:20:59 +0100

    Jogosultság ellenőrzés URL alapján

--- employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
+++ employees-standalone-form-spring-security/src/main/java/employees/SecurityConfig.java
@@ -2,8 +2,11 @@ package employees;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration(proxyBeanMethods = false)
 public class SecurityConfig {
@@ -11,5 +14,25 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests(registry ->
+                        // Sorrend
+                        // Specifikusabb szabályok kerüljenek felülre
+                        registry.requestMatchers("/login")
+                                .permitAll()
+                                .requestMatchers("/")
+                                .hasRole("USER")
+                                .requestMatchers("/create-employee")
+                                .hasRole("ADMIN")
+                                .anyRequest()
+                                .denyAll()
+                )
+                .formLogin(Customizer.withDefaults())
+                .logout(Customizer.withDefaults());
+        return http.build();
     }
 }
